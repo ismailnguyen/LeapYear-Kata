91b3a65dde3f85b0ec6815fcf597993ddc12a3af
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 12:23:19 +0100

    Refactor business

--- LeapYear-Kata/LeapYear.cs
+++ LeapYear-Kata/LeapYear.cs
@@ -4,9 +4,24 @@
     {
         public bool IsLeapYeart(int inputYear)
         {
-            return isDisivisblePer(inputYear, 4)
-                && !isDisivisblePer(inputYear, 100)
-                || (isDisivisblePer(inputYear, 100) && isDisivisblePer(inputYear, 400));
+            return isTypicalLeapYear(inputYear)
+                && !isAtypicalYear(inputYear)
+                || isAtypicalLeapYear(inputYear);
+        }
+
+        private bool isTypicalLeapYear(int inputYear)
+        {
+            return isDisivisblePer(inputYear, 4);
+        }
+
+        private bool isAtypicalYear(int inputYear)
+        {
+            return isDisivisblePer(inputYear, 100);
+
+        }
+        private bool isAtypicalLeapYear(int inputYear)
+        {
+            return isDisivisblePer(inputYear, 100) && isDisivisblePer(inputYear, 400);
         }
 
         private bool isDisivisblePer(int inputYear, int dividend)
