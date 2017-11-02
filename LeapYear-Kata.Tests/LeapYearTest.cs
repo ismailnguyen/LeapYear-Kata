5f7d06bf58d9033fde15ec0fc4317b191b3ed67f
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 11:31:22 +0100

    Initialize empty project

---
+++ LeapYear-Kata.Tests/LeapYearTest.cs
@@ -0,0 +1,14 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace LeapYear_Kata.Tests
+{
+    public class LeapYearTest
+    {
+        [Test]
+        public void TODO_Rename()
+        {
+            Check.That(true).IsTrue();
+        }
+    }
+}
