26d6ac9862396062160b456e06636a35f2ad5057
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 11:42:06 +0100

    2001 is a leap year

--- LeapYear-Kata.Tests/LeapYearTest.cs
+++ LeapYear-Kata.Tests/LeapYearTest.cs
@@ -5,9 +5,34 @@ namespace LeapYear_Kata.Tests
 {
     public class LeapYearTest
     {
-        [Test]
-        public void TODO_Rename()
+        /*
+            LEAP YEAR
+            Une méthode qui retourne vrai ou faux selon que l’entier en argument est une année bissextile ou non.
+            Une année bissextile est divisible par 4, mais n’est pas divisible par 100 à moins qu’elle soit aussi divisible par 400.
+            2001 est une année normale typique
+            1996 est une année bissextile typique
+            1900 est une année normale atypique
+            2000 est une année bissextile atypique
+        */
+
+        private LeapYear leapYear;
+
+        [SetUp]
+        public void Setup()
         {
+            leapYear = new LeapYear();
+        }
+
+        [Test]
+        public void Shoul_Return_True_When_Given_Year_Is_2001()
+        {
+            // GIVEN
+            int inputYear = 2001;
+
+            // WHEN
+            leapYear.IsLeapYeart(inputYear);
+
+            // THEN
             Check.That(true).IsTrue();
         }
     }
---
+++ LeapYear-Kata/LeapYear.cs
@@ -0,0 +1,10 @@
+﻿namespace LeapYear_Kata
+{
+    public class LeapYear
+    {
+        public bool IsLeapYeart(int inputYear)
+        {
+            return true;
+        }
+    }
+}
