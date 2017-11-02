e17dbe8caa8bfec0d7dfa840d447e1bb0f3661f8
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 11:56:04 +0100

    Refactor tests

--- LeapYear-Kata.Tests/LeapYearTest.cs
+++ LeapYear-Kata.Tests/LeapYearTest.cs
@@ -5,30 +5,12 @@ namespace LeapYear_Kata.Tests
 {
     public class LeapYearTest
     {
-        /*
-            LEAP YEAR
-            Une méthode qui retourne vrai ou faux selon que l’entier en argument est une année bissextile ou non.
-            Une année bissextile est divisible par 4, mais n’est pas divisible par 100 à moins qu’elle soit aussi divisible par 400.
-            2001 est une année normale typique
-            1996 est une année bissextile typique
-            1900 est une année normale atypique
-            2000 est une année bissextile atypique
-        */
+        private LeapYear leapYear = new LeapYear();
 
-        private LeapYear leapYear;
-
-        [SetUp]
-        public void Setup()
+        [TestCase(2001)]
+        [TestCase(1900)]
+        public void Shoul_False_When_Given_Year_Is_Not_Leap_Year(int inputYear)
         {
-            leapYear = new LeapYear();
-        }
-
-        [Test]
-        public void Shoul_False_True_When_Given_Year_Is_2001()
-        {
-            // GIVEN
-            int inputYear = 2001;
-
             // WHEN
             bool isLeapYear = leapYear.IsLeapYeart(inputYear);
 
@@ -36,38 +18,10 @@ namespace LeapYear_Kata.Tests
             Check.That(isLeapYear).IsFalse();
         }
 
-        [Test]
-        public void Shoul_Return_True_When_Given_Year_Is_1996()
+        [TestCase(1996)]
+        [TestCase(2000)]
+        public void Shoul_Return_True_When_Given_Year_Is_Leap_Year(int inputYear)
         {
-            // GIVEN
-            int inputYear = 1996;
-
-            // WHEN
-            bool isLeapYear = leapYear.IsLeapYeart(inputYear);
-
-            // THEN
-            Check.That(isLeapYear).IsTrue();
-        }
-
-        [Test]
-        public void Shoul_Return_False_When_Given_Year_Is_1900()
-        {
-            // GIVEN
-            int inputYear = 1900;
-
-            // WHEN
-            bool isLeapYear = leapYear.IsLeapYeart(inputYear);
-
-            // THEN
-            Check.That(isLeapYear).IsFalse();
-        }
-
-        [Test]
-        public void Shoul_Return_True_When_Given_Year_Is_2000()
-        {
-            // GIVEN
-            int inputYear = 2000;
-
             // WHEN
             bool isLeapYear = leapYear.IsLeapYeart(inputYear);
 
